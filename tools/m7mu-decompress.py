9df15b970690fb07efa44127b965c73676b1ca27
Georg Lukas <georg@op-co.de>
2025-04-26 18:01:00 +0200

    M7MU decompressor: WIP subsection/range decoder

--- tools/m7mu-decompress.py
+++ tools/m7mu-decompress.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import struct
 import sys
 from argparse import ArgumentParser
@@ -14,36 +15,60 @@ parser.add_argument('-o', '--output',
                     dest='fn_out', default=False,
                     help='output file to decompress into')
 parser.add_argument('-O', '--offset',
-                    type=auto_int, dest='offset', default=2,
+                    type=auto_int, dest='offset', default=0,
                     help='offset in input file to start decoding at')
 parser.add_argument('-C', '--count',
                     type=auto_int, dest='count', default=0,
                     help='number of blocks to decompress')
 parser.add_argument('-W', '--window',
-                    type=auto_int, dest='window', default=8192,
-                    help='window size for decompression')
+                    type=auto_int, dest='window', default=-1,
+                    help='window size for decompression (<0 - uncompressed stream; >0 - ring buffer)')
 parser.add_argument('-d', '--debug',
                     action='store_true', dest='debug', default=False,
                     help='emit debug output')
 args = parser.parse_args()
 
 OFS_BITS = 3
-window = bytearray(b'\x00' * args.window)
-window_idx = 0
+
+class RingBufferWindow(bytearray):
+    def __init__(self, winsize):
+        super().__init__(b'\x00'*winsize)
+        self.winsize = winsize
+        self.window_idx = 0
+
+    def extend(self, data):
+        for d in data:
+            self[self.window_idx] = d
+            self.window_idx = (self.window_idx + 1 ) % self.winsize
+
+    def get(self, offset, count):
+        result = bytearray()
+        for i in range(count):
+            result.append(self[(self.window_idx + self.winsize - offset + i) % self.winsize])
+        return result
+
+class AppendWindow(bytearray):
+    def __init__(self, winsize):
+        super().__init__()
+        self.winsize = winsize
+
+    def extend(self, data):
+        # TODO: remove the beginning to preserve RAM
+        super().extend(data)
+
+    def get(self, offset, count):
+        if offset == count:
+            end = None
+        else:
+            end = -offset+count
+        return self[-offset:end]
+
+if args.window < 0:
+    window = AppendWindow(-args.window)
+else:
+    window = RingBufferWindow(args.window)
 
 block_idx = 0
-
-def window_extend(data):
-    global window, window_idx
-    for d in data:
-        window[window_idx] = d
-        window_idx = (window_idx + 1 ) % args.window
-
-def window_get(offset, count):
-    result = bytearray()
-    for i in range(count):
-        result.append(window[(window_idx + args.window - offset + i) % args.window])
-    return result
 
 def x(ba):
     '''helper to output hex/readable output'''
@@ -62,7 +87,7 @@ def dprint(*a):
     if args.debug:
         print(*a)
 
-def decode_block(f, f_out=None):
+def decode_block(f, f_out=None, max_in=None):
     '''read a 16-bit flag from file f, read and decode the following data accordingly'''
     global block_idx
     ofs =f.tell()
@@ -70,20 +95,30 @@ def decode_block(f, f_out=None):
     flag = struct.unpack(">H", flag_bytes)[0]
     bitmask = format(flag, "016b")
     bitmask1 = bitmask + "1" # used to find() streaks of 0s
-    dprint(f"{ofs:08x} ({block_idx}): bitmask {xb(flag_bytes)}")
+    dprint(f"{ofs:08x} ({block_idx}): bitmask {xb(flag_bytes)} max={max_in}")
     block_idx = block_idx + 1
     i = 0
+    out_len = 0
     while i < 16:
+        _in = f.tell()-ofs
+        if _in >= max_in:
+            dprint(f"    EOB reached after reading {_in} and writing {out_len} bytes")
+            if _in > max_in:
+                f.seek(ofs+max_in)
+                dprint(f"    EOB rewinding!")
+            return out_len
         bit = bitmask[i]
         if bit == '0':
             # obtain number of consecutive 0 bits in flag -> number of literal bytes
             literal_len = bitmask1.find("1", i) - i
+            literal_len = min(literal_len, max_in-_in)
             literal = f.read(literal_len)
             dprint(f"    literal {x(literal)}")
-            window_extend(literal)
+            window.extend(literal)
             if f_out:
                 f_out.write(literal)
             i += literal_len
+            out_len += literal_len
         else:
             # read 2-byte flag and decode offset and length
             mask = f.read(2)
@@ -91,52 +126,89 @@ def decode_block(f, f_out=None):
             token_len = 3 + (ofshi_len & ((1 << OFS_BITS)-1))
             offset = ((ofshi_len >> OFS_BITS) << 8) + ofslo
             if token_len == 10:
-                # variable length token with additional length byte
-                vlb = f.read(1)
-                mask = mask + vlb
-                # the additional byte gets added to the length
-                token_len = token_len + vlb[0]
-                #if token_len > 128+10:
-                #    raise IndexError(f"{ofs:08x}: encountered token_len = {token_len}: {x(f.read(16))}")
-                if token_len == 265:
-                    raise IndexError(f"{ofs:08x}: encountered token_len = {token_len}: {x(f.read(16))}")
-                    #token_len = token_len + f.read(1)[0]
-                    #if token_len == 255 + 265:
-                    #    token_len = token_len + f.read(1)[0]
-                    #    if token_len == 255 + 255 + 265:
-                    #        raise IndexError(f"{ofs:08x}: encountered token_len = {token_len}: {x(f.read(16))}")
+                # variable length token with additional length bytes: read until != 255
+                vlb = 255
+                while vlb == 255:
+                    if _in+len(mask) >= max_in:
+                        dprint(f"    can't read token byte #{len(mask)} due to EOB")
+                        break
+                    vlb = f.read(1)[0]
+                    mask = mask + bytes([vlb])
+                    # the additional byte gets added to the length
+                    token_len = token_len + vlb
             dprint(f"    token   {xb(mask)}: {token_len} at -{offset}")
             #TODO: can we use this to validate "good" vs. bad offsets?
             if offset > len(window):
                 raise IndexError(f"{ofs:08x}: trying to look back {offset} bytes in a {len(window)}-sized window")
-            # extend the window byte-by-byte to prevent feeding old bytes when offset < token_len
+            # VARIANT A: first get the bytes from the old window, then extend the window
+            #lookup = window.get(offset, token_len)
+            #window.extend(lookup)
+            # VARIANT B: extend the window byte-by-byte to prevent feeding old bytes when offset < token_len
             lookup = bytearray()
             for l in range(token_len):
-                nxt = window_get(offset, 1)
+                nxt = window.get(offset, 1)
                 lookup.extend(nxt)
-                window_extend(nxt)
+                window.extend(nxt)
             #if offset < token_len:
             #    raise IndexError(f"{ofs:08x}: trying to look back {offset} bytes for a {token_len} token")
             dprint(f"        --> {x(bytes(lookup))}")
-            #dprint(f"        --> {x(bytes(lookup))} -- {x(window_get(offset, 16))}")
+            #dprint(f"        --> {x(bytes(lookup))} -- {x(window.get(offset, 16))}")
             if f_out:
                 f_out.write(bytes(lookup))
             i += 1
+            out_len += len(lookup)
+    return out_len
 
+def decode_range(f, f_out=None):
+    '''decode a subsection from a file, starting with a flag+length, then (un)compressed, then EOB=0x0000'''
+    f_pos = f.tell()
+    range_len_b = f.read(2)
+    range_len = struct.unpack(">H", range_len_b)[0]
+    if range_len == 0x0000:
+        dprint(f"*** {f_pos:08x} EOF marker {x(range_len_b)} ***")
+        raise struct.error("TODO hax")
+    elif range_len >= 0x8000:
+        # copy uncompressed section
+        data_len = (range_len & 0x7fff)-2
+        if data_len == 0x5ffe:
+            # special case (range_len=0xe000) = max length block with no EOB
+            data_len += 2
+        dprint(f"*** {f_pos:08x} Copying uncompressed section of {data_len} bytes ***")
+        if range_len == 0x8000: # special case for Galaxy K Zoom
+            raise IndexError(f"{f_pos:08x} Boom! Unsupported range_len = 0x{range_len:04x}")
+        if f_out:
+            os.sendfile(f_out.fileno(), f.fileno(), f_pos+2, data_len)
+        f.seek(f_pos + data_len + 2)
+        if data_len == 0x6000:
+            # skip missing EOB
+            return
+    else:
+        dprint(f"*** {f_pos:08x} Decoding compressed section of {range_len} bytes ***")
+        next_offset = f_pos + range_len
+        out_len = 0
+        while f_pos < next_offset:
+        #while out_len < range_len:
+            out_len += decode_block(f, f_out, next_offset - f_pos)
+            f_pos = f.tell()
+            if f_pos > next_offset:
+                raise IndexError(f"{f_pos:08x} Boom! Read {f_pos-next_offset} bytes beyond {next_offset:08x}")
+            #if out_len > range_len:
+                #raise IndexError(f"{f_pos:08x} Boom! Expected {range_len} but end up at {out_len}")
+        dprint(f"*** {f_pos:08x} wrote {out_len} bytes")
+    f_pos = f.tell()
+    eob = f.read(2)
+    if eob != b"\x00\x00":
+        raise IndexError(f"{f_pos:08x} Boom! Expected EOB, got {x(eob)}")
 
-f_in = open(args.filename, "rb")
-f_in.seek(args.offset)
+f = open(args.filename, "rb")
+f.seek(args.offset)
 if args.fn_out:
     f_out = open(args.fn_out, "wb")
 else:
     f_out = None
 
 try:
-    if args.count:
-        for i in range(args.count):
-            decode_block(f_in, f_out)
-    else:
-        while True:
-            decode_block(f_in, f_out)
+    while True:
+        decode_range(f, f_out)
 except struct.error:
     pass
